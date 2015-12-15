3fa3370029eb070b8bbb9ca10e04379873398ce9
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2015-12-15 11:52:43 +0200

    1004: tag, location_tag, webuser tables

--- database/drop-mestat.sql
+++ database/drop-mestat.sql
@@ -1,3 +1,6 @@
 
 drop table if exists location;
+drop table if exists tag;
+drop table if exists location_tag;
+drop table if exists webuser;
 
--- database/mestat.sql
+++ database/mestat.sql
@@ -8,3 +8,21 @@ create table if not exists location (
 
 create index location_coord_key on location using gist (coord);
 
+create table if not exists tag (
+	id serial primary key,
+	name text not null,
+	ns text not null,
+	unique (ns, name)
+);
+
+create table if not exists location_tag (
+	location integer not null,
+	tag integer not null
+);
+
+create table if not exists webuser (
+	id serial primary key,
+	username text not null unique,
+	identification text
+);
+
