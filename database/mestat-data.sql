227955295539249aedefa9e52fa8c4e1fd1c11d3
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2015-12-15 14:04:02 +0200

    add the anonymous user

--- database/mestat-data.sql
+++ database/mestat-data.sql
@@ -1,5 +1,5 @@
 
-insert into webuser(username) values ('system'), ('testuser');
+insert into webuser(username) values ('system'), ('anonymous'), ('testuser');
 insert into location(coord) values (point '(60.16952, 24.93545)');
 insert into tag(name, ns) values ('city', 'testuser'), ('capital', 'testuser');
 insert into location_tag(location, tag) values (1, 1), (1, 2);
