244cd76dfe6b52f899fbf162c28433bce1d0400e
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2015-12-16 12:43:12 +0200

    more test data

--- database/mestat-data.sql
+++ database/mestat-data.sql
@@ -1,6 +1,15 @@
 
 insert into webuser(username) values ('system'), ('anonymous'), ('testuser');
-insert into location(coord) values (point '(60.16952, 24.93545)');
-insert into tag(name, ns) values ('city', 'testuser'), ('capital', 'testuser');
-insert into location_tag(location, tag) values (1, 1), (1, 2);
+
+insert into location(coord) values
+ (point '(24.93545,60.16952)'),
+ (point '(25.46816,65.01236)'),
+ (point '(31.5,61.0)');
+
+insert into tag(name, ns) values
+ ('deleted', 'system'),
+ ('city', 'testuser'),
+ ('capital', 'testuser');
+
+insert into location_tag(location, tag) values (1, 2), (1, 3), (2, 2);
 
