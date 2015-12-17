1e661a0fb2049d558016a5612e836edcb3eec7b1
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2015-12-17 15:34:50 +0200

    1003: improve error reporting, still needs to report bad params

--- mestat-app/src/mestat_app/handler.clj
+++ mestat-app/src/mestat_app/handler.clj
@@ -6,23 +6,30 @@
             [ring.middleware.format :refer [wrap-restful-format]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn str->float [s]
+  (and s (try (Float/parseFloat s) (catch NumberFormatException _ nil))))
+
 (def search-handler
   (GET "/search" [long lat limit page]
-       (and long lat
-            (try (model/points-near (pg/point (Float/parseFloat long)
-                                              (Float/parseFloat lat))
-                                    :limit (or limit 25)
-                                    :page (or page 0))
-                 (catch NumberFormatException _ nil)))))
+       (let [x (str->float long)
+             y (str->float lat)
+             limit (str->float limit)
+             page (str->float page)]
+         (and x y (model/points-near (pg/point x y)
+                                     :limit (or limit 25)
+                                     :page (or page 0))))))
+
+(def test-handler
+  (GET "/test" [] (list {:message "yes, it works"})))
 
 (defroutes app-routes
   (GET "/" [] "<p>Hello World</p>\n")
   (wrap-restful-format
     (context "/api/v1" []
              search-handler
-             (route/not-found {:status "error", :message "not found",
-                               :description "this API call does not exist"})))
-  (route/not-found "Not Found"))
+             test-handler
+             (route/not-found "this API call does not exist\n")))
+  (route/not-found "Not Found\n"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
