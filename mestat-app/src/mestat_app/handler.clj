0eb6fa27b7b6d89f17ed8f45fa6ba702ded7b2a8
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2015-12-17 15:00:32 +0200

    1003: better error handling and parameter conversions, limit and page params, tests pass

--- mestat-app/src/mestat_app/handler.clj
+++ mestat-app/src/mestat_app/handler.clj
@@ -6,13 +6,22 @@
             [ring.middleware.format :refer [wrap-restful-format]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(def search-handler
+  (GET "/search" [long lat limit page]
+       (and long lat
+            (try (model/points-near (pg/point (Float/parseFloat long)
+                                              (Float/parseFloat lat))
+                                    :limit (or limit 25)
+                                    :page (or page 0))
+                 (catch NumberFormatException _ nil)))))
+
 (defroutes app-routes
   (GET "/" [] "<p>Hello World</p>\n")
   (wrap-restful-format
     (context "/api/v1" []
-      (GET "/search" [long lat]
-           (model/points-near (pg/point (Float/parseFloat long)
-                                        (Float/parseFloat lat))))))
+             search-handler
+             (route/not-found {:status "error", :message "not found",
+                               :description "this API call does not exist"})))
   (route/not-found "Not Found"))
 
 (def app
