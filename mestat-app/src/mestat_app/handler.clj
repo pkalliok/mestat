0efc4e197d5655fb249f9f8e38195503a6b07bc0
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2015-12-18 11:23:49 +0200

    1003: proper error reporting, tests for them, tests pass (done)

--- mestat-app/src/mestat_app/handler.clj
+++ mestat-app/src/mestat_app/handler.clj
@@ -3,11 +3,16 @@
             [compojure.route :as route]
             [mestat-app.model :as model]
             [clj-postgresql.core :as pg]
+            [ring.util.response :as response]
             [ring.middleware.format :refer [wrap-restful-format]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defn str->float [s]
   (and s (try (Float/parseFloat s) (catch NumberFormatException _ nil))))
+
+(def ok response/response)
+(defn status [status body]
+  (response/status (ok body) status))
 
 (def search-handler
   (GET "/search" [long lat limit page]
@@ -15,12 +20,14 @@
              y (str->float lat)
              limit (str->float limit)
              page (str->float page)]
-         (and x y (model/points-near (pg/point x y)
-                                     :limit (or limit 25)
-                                     :page (or page 0))))))
+         (or (and x y (ok (model/points-near (pg/point x y)
+                                             :limit (or limit 25)
+                                             :page (or page 0))))
+             (status 400 {:error "missing search parameters"
+                          :missing '(long lat)})))))
 
 (def test-handler
-  (GET "/test" [] (list {:message "yes, it works"})))
+  (GET "/test" [] (ok {:message "yes, it works"})))
 
 (defroutes app-routes
   (GET "/" [] "<p>Hello World</p>\n")
@@ -28,7 +35,8 @@
     (context "/api/v1" []
              search-handler
              test-handler
-             (route/not-found "this API call does not exist\n")))
+             (route/not-found
+               (status 404 {:error "this API call does not exist\n"}))))
   (route/not-found "Not Found\n"))
 
 (def app
