706213bbb971fb1354135725d9f9b56c77d65bf7
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2015-12-23 11:56:10 +0200

    1002: tests for point and tag models

--- mestat-app/test/mestat_app/model_test.clj
+++ mestat-app/test/mestat_app/model_test.clj
@@ -4,7 +4,18 @@
             [mestat-app.model :refer :all]))
 
 (deftest test-model
-  (testing "point conversions")
+  (testing "coordinate conversions"
+    (let [co1 (any->coord "(15,20.73)")
+          co2 (any->coord [15 20.73])]
+      (is (= co1 co2))
+      (is (= (coord-long co1) 15.0))
+      (is (= (coord-lat co2) 20.73))))
+  (testing "tag conversions"
+    (let [tag1 (make-tag "foo" "bar")
+          tag2 (make-tag "quux" "bar")]
+      (is (= (tag-name tag1) (tag-name tag2)))
+      (is (not= tag1 tag2))
+      (is (= (tag-ns tag2) "quux"))))
   (testing "points by proximity"
     (let [coord [25.46816 65.01236]
           p (points-near coord)]
